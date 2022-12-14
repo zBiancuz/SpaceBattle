6f8ffdcf3a06d1533178cd29d498f4425b42c7c0
zBiancuz <teobryant2003@gmail.com>
2022-12-14 11:02:08 +0100

    NuovoBottone

--- SpaceBattle/MenuGioco.xaml.cs
+++ SpaceBattle/MenuGioco.xaml.cs
@@ -23,5 +23,10 @@ namespace SpaceBattle
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
